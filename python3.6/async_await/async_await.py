4f01482e081416f34066714fce3bf50a1d64fc7b
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-02-22 00:31:07 +0530

    refine async/await example

--- python3.6/async_await/async_await.py
+++ python3.6/async_await/async_await.py
@@ -4,25 +4,27 @@ import asyncio
 import httpx
 
 start = time.time()
-requests.get("http://google.com")
-requests.get("http://google.com")
-requests.get("http://google.com")
+requests.get("https://www.google.com")
+requests.get("https://www.google.com")
+requests.get("https://www.google.com")
 end = time.time()
-print("time taken to hit google three times: ", end - start)
+print("time taken to hit google three times consecutively: ", end - start)
 
 # Now do the same thing as before but asynchronously so that it's quicker
-
-# TODO: modify this method to actually print the responses so we know it's working
 async def fetch_web_page_asynchronously():
     async with httpx.AsyncClient() as client:
         # create a list of coroutines/tasks
         # there's three tasks that fetch google.com
-        tasks = [client.get("http://google.com") for _ in range(3)]
+        tasks = [client.get("https://www.google.com") for _ in range(3)]
         
-        # TODO: figure out what await actually means
+        # TODO: figure out what await actually means -> it's different compared to join threads in java
         responses = await asyncio.gather(*tasks)
+        
+        for i, response in enumerate(responses):
+            print(f"Response status code {i}:")
+            print(response.status_code)
     
 start = time.time()
 asyncio.run(fetch_web_page_asynchronously())
 end = time.time()
-print("time taken to hit google three times: ", end - start)
+print("time taken to hit google three times concurrently: ", end - start)
